c62dc5a78eff1abbb0320926810a8635211ecdaf
Jean-Ray Arseneau <jarsenea@arseneau.ca>
2014-11-30 21:27:31 -0500

    Updated start.sh.

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ set -e
 
 [ ! -f /volumes/config/nzbget.conf ] && cp /usr/local/share/nzbget/nzbget.conf /volumes/config/nzbget.conf
 
-nzbget --configfile /volumes/config/nzbget.conf
+nzbget --configfile /volumes/config/nzbget.conf --daemon
